d52c4f51598a5580cd168e717b759bcef1b59e9c
mid <mid@k302.kcsk.ac.jp>
2025-07-15 12:05:21 +0900

    testadd

---
+++ src/ReturnPanel.java
@@ -0,0 +1,32 @@
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import jp.ac.kcs.swing.library.DB;
+import jp.ac.kcs.swing.library.MainFrame;
+
+public class ReturnPanel extends JPanel {
+	public ReturnPanel(MainFrame frame) {
+        //ボタンとか作成
+    	setLayout (new GridLayout(2, 2));
+    	JTextField dvdCodeField = new JTextField(); 
+    	JButton button1=new JButton("返却");
+    	JButton button2=new JButton("TOPへ戻る");
+    	//DB返却処理
+    	button1.addActionListener(e -> {
+        	DB.returnBook(dvdCodeField.getText());
+        	JOptionPane.showMessageDialog(this, "返却しました。");
+        	});
+    	//TOPに戻るボタン
+    	button2.addActionListener(e -> frame.showPanel("TOP"));
+    	//パネルに部品の追加
+    	add(new JLabel("DVDコード"));
+    	add(dvdCodeField);
+    	add(button1); //返却
+    	add(button2); //topへ戻る
+    }
+}
