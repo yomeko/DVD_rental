cdf7ea71e0137087e6897ebebbe6a8c3b4b414e4
mid <mid@k302.kcsk.ac.jp>
2025-07-16 11:21:56 +0900

    testadd

---
+++ src/RentPanel.java
@@ -0,0 +1,35 @@
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class RentPanel extends JPanel {
+	public RentPanel(MainFrame frame) {
+		CardLayout cardLayout = new CardLayout();
+		frame.add(frame, "main");
+		//ボタンとかフィールド作成
+    	setLayout (new GridLayout(3, 2));
+    	JTextField idField = new JTextField();
+    	JTextField codeField = new JTextField();
+    	JButton button1=new JButton("貸出");
+    	JButton button2=new JButton("TOPへ戻る");
+    	//DB実行
+    	button1.addActionListener(e -> {
+        	DB.lendDVD(idField.getText(),( codeField.getText()));
+        	JOptionPane.showMessageDialog(this, "貸出しました。");
+        	});
+        //TOP戻る
+    	button2.addActionListener(e -> cardLayout.show(frame, "main"));
+        //部品追加
+    	add(new JLabel("会員ID"));
+    	add(idField);
+    	add(new JLabel("DVDコード"));
+    	add(codeField);
+    	add(button1); //貸出
+    	add(button2); //top
+    }
+}
