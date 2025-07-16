c6a31201907790f4aed4c05d92df3def443b3fed
mid <mid@k302.kcsk.ac.jp>
2025-07-16 11:21:14 +0900

    testadd

--- src/LendingPanel.java
+++ src/LendingPanel.java
@@ -1,35 +1,31 @@
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.GridLayout;
+import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class LendingPanel extends JPanel {
 	public LendingPanel(MainFrame frame) {
 		CardLayout cardLayout = new CardLayout();
 		frame.add(frame, "main");
-		//ボタンとかフィールド作成
-    	setLayout (new GridLayout(3, 2));
-    	JTextField idField = new JTextField();
-    	JTextField codeField = new JTextField();
-    	JButton button1=new JButton("貸出");
-    	JButton button2=new JButton("TOPへ戻る");
-    	//DB実行
-    	button1.addActionListener(e -> {
-        	DB.lendDVD(idField.getText(),( codeField.getText()));
-        	JOptionPane.showMessageDialog(this, "貸出しました。");
-        	});
-        //TOP戻る
-    	button2.addActionListener(e -> cardLayout.show(frame, "main"));
-        //部品追加
-    	add(new JLabel("会員ID"));
-    	add(idField);
-    	add(new JLabel("DVDコード"));
-    	add(codeField);
-    	add(button1); //貸出
-    	add(button2); //top
+        //レイアウト追加
+        setLayout(new BorderLayout());
+        //表示フィールド追加
+        JTextArea area = new JTextArea();
+        area.setEditable(false);
+        //Listを作成string型
+        List<String>dvds = DB.getLentDVDs();
+        for(String dvd:dvds) {
+        	area.append(dvd+"\n");
+        }
+        //TOP戻るボタン
+        JButton button2=new JButton("TOPへ戻る");
+        button2.addActionListener(e -> cardLayout.show(frame, "main"));
+        //パネルに部品の追加
+        add(new JScrollPane(area),BorderLayout.CENTER);
+        add(button2, BorderLayout.SOUTH);
     }
 }
